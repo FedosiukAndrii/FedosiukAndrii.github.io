4e345002b5d692b4ab92641113e6de0e7b7af04c
Andrii Fedosiuk <17waw82@gmail.com>
2021-12-08 15:10:55 +0200

    Updates

---
+++ index.17eaaabf.js
@@ -0,0 +1,2 @@
+const updateDate=function(){let t=new Date;document.querySelector(".date-container").textContent=t.getDay()+"."+t.getMonth()+"."+t.getFullYear()+"  "+t.getHours()+":"+t.getMinutes()}()();
+//# sourceMappingURL=index.17eaaabf.js.map
---
+++ index.7074e8aa.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelector("#like");e.addEventListener("click",(()=>e.style.color="red"))}();
+//# sourceMappingURL=index.7074e8aa.js.map
